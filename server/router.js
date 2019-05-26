36d2fd629cdd2e8e442c21c86f6781fafeca2d43
annix8 <mario.ivailov@abv.bg>
2019-05-26 15:28:37 +0300

    code cleanup

--- server/router.js
+++ server/router.js
@@ -14,26 +14,38 @@ class Router {
 
         fs.readdirSync(folderName)
             .forEach(file => {
-                const fullName = path.join(folderName, file);
-                const isDir = fs.lstatSync(fullName).isDirectory();
+                const fullFileName = path.join(folderName, file);
+                const isDir = fs.lstatSync(fullFileName).isDirectory();
+
                 if (isDir) {
-                    this.loadRoutes(server, fullName);
+                    this.loadRoutes(server, fullFileName);
                 }
-                else if (file.toLowerCase().indexOf('.js')) {
-                    const dirs = path.dirname(fullName).split(path.sep);
-                    const startFolderIndex = dirs.indexOf(this.startFolder);
-                    if(startFolderIndex > -1){
-                        dirs.splice(startFolderIndex, 1);
-                    }
-                    
-                    const controllerClass = require(`./${fullName}`);
-                    const baseRoute = '/' + dirs.join('/');
+                else if (isJsFile(file)) {
+                    const controllerClass = require(`./${fullFileName}`);
                     const router = express.Router();
+
                     const controller = new controllerClass(router);
+
+                    const baseRoute = getBaseRouteDir(fullFileName, this.startFolder);
                     server.use(baseRoute, router);
                     console.log(`Generated api url: ${baseRoute}`);
                 }
             });
+
+
+        function isJsFile(file) {
+            return file.toLowerCase().indexOf('.js');
+        }
+
+        function getBaseRouteDir(fileFullName, startFolder) {
+            const dirs = path.dirname(fileFullName).split(path.sep);
+            const startFolderIndex = dirs.indexOf(startFolder);
+            if (startFolderIndex > -1) {
+                dirs.splice(startFolderIndex, 1);
+            }
+
+            return '/' + dirs.join('/');
+        }
     }
 }
 
