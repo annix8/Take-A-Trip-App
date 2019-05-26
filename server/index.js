de4240a8ea832340b62eefb539fed1a7a632c62d
annix8 <mario.ivailov@abv.bg>
2019-05-26 14:30:23 +0300

    added nodemon script command, server now starts

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const port = 3200;
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send("Server is working");
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+});
