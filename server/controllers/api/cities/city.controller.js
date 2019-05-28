935add19062c01ec7cef5e4f64d07fc8cec72825
annix8 <mario.ivailov@abv.bg>
2019-05-28 21:11:19 +0300

    added cities get by name

--- server/controllers/api/cities/city.controller.js
+++ server/controllers/api/cities/city.controller.js
@@ -1,19 +1,33 @@
 const City = require('../../../models/city');
 
-class CityController{
-    constructor(router){
+class CityController {
+    constructor(router) {
         router.get('/', this.getAll.bind(this));
+        router.get('/:name', this.getByName.bind(this))
     }
 
-    getAll(req, res){
-        City.find((err, data) =>{
-            if(err){
-                return res.send(err);
-            }
-
-            return res.json(data);
+    getAll(req, res) {
+        City.find((err, cities) => {
+            return handleResponse(res, err, cities);
         });
     }
+
+    getByName(req, res) {
+        const query = {
+            name: req.params.name
+        };
+        City.find(query, (err, cities) => {
+            return handleResponse(res, err, cities);
+        });
+    }
+}
+
+function handleResponse(res, err, data) {
+    if (err) {
+        return res.send(err);
+    }
+
+    return res.json(data);
 }
 
 module.exports = CityController;
