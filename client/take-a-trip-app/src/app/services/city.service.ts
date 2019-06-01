a88a0a7dcaa521eb84f2385126d65c006ceee447
annix8 <mario.ivailov@abv.bg>
2019-06-01 19:54:04 +0300

    added simple error handling in city.service

--- client/take-a-trip-app/src/app/services/city.service.ts
+++ client/take-a-trip-app/src/app/services/city.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators'; 
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { ICity } from '../models/city';
 
@@ -15,6 +15,24 @@ export class CityService {
 
   getAll(includePlaces: boolean = false): Observable<ICity[]> {
     const citiesUrl = this.baseUrl + "/cities";
-    return this.http.get<ICity[]>(citiesUrl);
+
+    return this.http.get<ICity[]>(citiesUrl)
+      .pipe(
+        catchError(this.handleError<ICity[]>("Get cities", []))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error);
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
