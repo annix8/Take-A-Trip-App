b5a27d967f73b6388c05dd8ed5fe5f6b8cda078c
annix8 <mario.ivailov@abv.bg>
2019-06-01 19:49:21 +0300

    api url is taken from environment file

--- client/take-a-trip-app/src/app/services/city.service.ts
+++ client/take-a-trip-app/src/app/services/city.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators'; 
+import { environment } from '../../environments/environment';
 import { ICity } from '../models/city';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
-  // TODO take from config
-  private citiesUrl = "http://localhost:3200/api/cities";
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getAll(includePlaces: boolean = false): Observable<ICity[]> {
-    return this.http.get<ICity[]>(this.citiesUrl);
+    const citiesUrl = this.baseUrl + "/cities";
+    return this.http.get<ICity[]>(citiesUrl);
   }
 }
