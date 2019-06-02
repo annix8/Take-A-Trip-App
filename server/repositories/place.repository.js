c88a6e497a189a0aa1188fb9d3e77f7a20fa0e3d
annix8 <mario.ivailov@abv.bg>
2019-06-02 19:23:26 +0300

    places api now returns images as urls

--- server/repositories/place.repository.js
+++ server/repositories/place.repository.js
@@ -1,9 +1,18 @@
 const Place = require('../models/place');
 
 class PlaceRepository {
-    get(query, callback){
-        Place.find(query, (err, placeResponse) =>{
-            callback(err, placeResponse);
+    get(query, callback) {
+        Place.find(query, (err, placesResponse) => {
+            const placesJson = placesResponse.map(place => {
+                const newPlace = place.toJSON();
+                newPlace.images = newPlace.images.map(imageId => {
+                    return `http://localhost:3200/api/images/${imageId._id}`;
+                });
+
+                return newPlace;
+            });
+
+            callback(err, placesJson);
         });
     }
 }
