9de76f65a3e458ab74656d7ec1e4c0efd11acd22
annix8 <mario.ivailov@abv.bg>
2019-06-06 23:05:20 +0300

    added forms module

--- client/take-a-trip-app/src/app/app.module.ts
+++ client/take-a-trip-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { AdminComponent } from './components/admin/admin.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
