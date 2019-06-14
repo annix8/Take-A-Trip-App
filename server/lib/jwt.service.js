0d7d5b9aad42a6e4e9c454527bae1d3a44c27072
annix8 <mario.ivailov@abv.bg>
2019-06-15 00:23:58 +0300

    jwt is validated synchronously

--- server/lib/jwt.service.js
+++ server/lib/jwt.service.js
@@ -2,27 +2,25 @@ const jwt = require('jsonwebtoken');
 
 const jwt_secret = "jwt_secret";
 
-class JwtService{
-    signToken(payload){
+class JwtService {
+    signToken(payload) {
         return jwt.sign(payload, jwt_secret);
     }
 
-    validateToken(token){
-        jwt.verify(token, jwt_secret, function(err, decoded) {
-            let result = {};
-            if(err){
-                result.error = {
-                    ...err
-                };
-            }
-            else{
-                result = {
-                    ...decoded
-                };
-            }
-
-            return result;
-       });
+    validateToken(token) {
+        try {
+            const payload = jwt.verify(token, jwt_secret);
+            return {
+                success: true,
+                payload: payload
+            };
+        }
+        catch (err) {
+            return {
+                success: false,
+                error: err.message
+            };
+        }
     }
 }
 
