4de4f661d9df86d2d23a40e4a1c218b8a2e3521c
annix8 <mario.ivailov@abv.bg>
2019-06-15 00:49:06 +0300

    added user schema

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true, unique: true, required: [true, "can't be blank"], index: true },
+        email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
+        password: { type: String, required: true }
+    }
+);
+
+module.exports = mongoose.model('User', userSchema, 'users');
