3118801110f202ea19a6b7efd432bd99ebff0ca3
annix8 <mario.ivailov@abv.bg>
2019-06-15 20:53:04 +0300

    added auth guard

--- client/take-a-trip-app/src/app/app-routing.module.ts
+++ client/take-a-trip-app/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { CityDetailsComponent } from './components/city-details/city-details.com
 import { PlaceDetailsComponent } from './components/place-details/place-details.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthenticationGuard } from './guards/authentication.guard';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: WelcomeComponent },
   { path: "login", component: LoginComponent },
   { path: "city/:id", component: CityDetailsComponent },
   { path: "place/:id", component: PlaceDetailsComponent },
-  { path: "admin", component: AdminComponent },
+  { path: "admin", component: AdminComponent, canActivate: [AuthenticationGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
