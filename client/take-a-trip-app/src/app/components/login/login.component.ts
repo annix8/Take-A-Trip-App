8dcc17f1686fb58c97eed1dc228a44f20fdf1288
annix8 <mario.ivailov@abv.bg>
2019-06-15 19:54:45 +0300

    first steps in logging in

--- client/take-a-trip-app/src/app/components/login/login.component.ts
+++ client/take-a-trip-app/src/app/components/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  isSuccess: boolean | null;
+  message: string;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
   }
 
+  onSubmit(username, password) {
+    this.authService.login(username, password)
+      .subscribe(
+        err => {
+          this.isSuccess = false;
+          this.message = JSON.stringify(err);
+        },
+        result => {
+          this.isSuccess = true;
+          this.message = JSON.stringify(result);
+        }
+      )
+  }
+
+  closeAlert(){
+    this.isSuccess = null;
+  }
 }
--- client/take-a-trip-app/src/app/services/authentication.service.ts
+++ client/take-a-trip-app/src/app/services/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { handleHttpError } from '../util/http-util';
 import { Observable } from 'rxjs';
@@ -10,11 +10,16 @@ import { Observable } from 'rxjs';
 })
 export class AuthenticationService {
   baseUrl = environment.apiUrl + "/auth";
+  defaultHttpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
 
   constructor(private http: HttpClient) { }
 
   login(username, password) : Observable<any> {
-    return this.http.post(this.baseUrl, { username, password })
+    return this.http.post(this.baseUrl + "/login", { username: username, password: password }, this.defaultHttpOptions)
       .pipe(
         catchError(handleHttpError("Login", null))
       );
