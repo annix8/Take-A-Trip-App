7b90ad6f00af23496b054f827d8a8f86c097353f
annix8 <mario.ivailov@abv.bg>
2019-06-15 18:18:06 +0300

    added requireJwt middleware in util.js

--- server/util.js
+++ server/util.js
@@ -1,4 +1,21 @@
+const jwtService = require('./services/jwt.service');
+
 class Util {
+    requireJwt(req, res, next) {
+        const auth = req.headers.authorization;
+        if (!auth) {
+            return res.status(401).send({ error: "Missing json web token." });
+        }
+        else {
+            const token = auth.split(' ')[1];
+            const isTokenValid = jwtService.validateToken(token);
+            if (!isTokenValid.sucess) {
+                return res.status(401).send({ error: "Invalid json web token" });
+            }
+            return next();
+        }
+    }
+
     handleJsonResponse(res, err, data) {
         if (err) {
             return res.send(err);
@@ -23,7 +40,7 @@ class Util {
                 .split(',')
                 .map(x => `-${x}`);
         }
-    
+
         return excludeParams;
     }
 }
