a051bd93fea911d7cdf1db2286a5fb6870740c7a
annix8 <mario.ivailov@abv.bg>
2019-06-21 22:42:52 +0300

    user info added to jwt response

--- server/services/authentication.service.js
+++ server/services/authentication.service.js
@@ -23,13 +23,7 @@ class AuthenticationService {
                         callback(invalidCredentialsObj);
                     }
                     else {
-                        const jwtBearerToken = jwtService.signToken({
-                            username: user.username
-                        });
-                        const result = {
-                            success: true,
-                            token: jwtBearerToken
-                        };
+                        const result = createSuccessResult(user);
                         callback(null, result);
                     }
                 }
@@ -44,17 +38,32 @@ class AuthenticationService {
                     callback(err);
                 }
                 else {
-                    const jwtBearerToken = jwtService.signToken({
-                        username: user.username
-                    });
-                    const result = {
-                        success: true,
-                        token: jwtBearerToken
-                    };
+                    const result = createSuccessResult(user);
                     callback(null, result);
                 }
             });
     }
+}
+
+function createSuccessResult(user){
+    const userData = createUserInfoForJwt(user);
+    const jwtBearerToken = jwtService.signToken(userData);
+    const result = {
+        success: true,
+        token: jwtBearerToken
+    };
+
+    return result;
+}
+
+function createUserInfoForJwt(user){
+    const userObj = {
+        username: user.username,
+        email: user.email,
+        user_id: user._id
+    };
+
+    return userObj;
 }
 
 module.exports = new AuthenticationService();
