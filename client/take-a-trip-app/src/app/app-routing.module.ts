037f8ff0c3d6ee960e74b01d745ab2f4d8e5a74b
annix8 <mario.ivailov@abv.bg>
2019-06-21 22:25:38 +0300

    removed admin component that was previously transformed into create place component

--- client/take-a-trip-app/src/app/app-routing.module.ts
+++ client/take-a-trip-app/src/app/app-routing.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { AuthenticationGuard } from './guards/authentication.guard';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: WelcomeComponent },
-  { path: "admin", component: AdminComponent, canActivate: [AuthenticationGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
