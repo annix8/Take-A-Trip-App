fdf9d7a46ce781c41ffaa90514a4595d431d103b
annix8 <mario.ivailov@abv.bg>
2019-06-22 22:27:29 +0300

    user service extends base service

--- client/take-a-trip-app/src/app/services/user.service.ts
+++ client/take-a-trip-app/src/app/services/user.service.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { handleHttpError } from '../util/http-util';
-import { environment } from 'src/environments/environment';
-import { catchError } from 'rxjs/operators';
+import { ServiceBase } from './service-base';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
-  baseUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) { }
+export class UserService extends ServiceBase {
 
   getUserRatingForPlace(userId: string, placeId: string) {
     const url = this.baseUrl + `/users/${userId}/ratingForPlace/${placeId}`;
-    return this.http.get<number>(url)
-      .pipe(
-        catchError(handleHttpError<number>("Get user rating for place", null))
-      )
+
+    return super.get<number>(url);
   }
 }
