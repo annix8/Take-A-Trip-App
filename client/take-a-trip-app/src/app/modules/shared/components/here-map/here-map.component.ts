8b9e55468a0199a602df481ad0836674dc33af2e
annix8 <mario.ivailov@abv.bg>
2019-07-14 22:00:47 +0300

    integrated interactive here map

--- client/take-a-trip-app/src/app/modules/shared/components/here-map/here-map.component.ts
+++ client/take-a-trip-app/src/app/modules/shared/components/here-map/here-map.component.ts
@@ -8,7 +8,8 @@ declare var H: any;
   styleUrls: ['./here-map.component.scss']
 })
 export class HereMapComponent implements OnInit {
-
+  private ui: any;
+  private map: any;
   @ViewChild("map") public mapElement: ElementRef;
 
   constructor() { }
@@ -21,14 +22,30 @@ export class HereMapComponent implements OnInit {
       "apikey": "M00YI3E6cEVA4sMcOBo7LZXPwWMuzenfsU0JcB7QqmU"
     });
     let defaultLayers = platform.createDefaultLayers();
-    let map = new H.Map(
+    this.map = new H.Map(
       this.mapElement.nativeElement,
       defaultLayers.vector.normal.map,
       {
         zoom: 10,
-        center: { lat: 42.695808, lng: 23.332794 }
+        center: { lat: 37.7397, lng: -121.4252 }
       }
     );
+    let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    this.ui = H.ui.UI.createDefault(this.map, defaultLayers);
+
+    this.dropMarker({ "lat": 42.695808, "lng": 23.332794 }, "Saint Alexander Nevsky");
+  }
+
+  private dropMarker(coordinates: any, title: string) {
+    let marker = new H.map.Marker(coordinates);
+    marker.setData("<p>" + title + "</p>");
+    marker.addEventListener('tap', event => {
+      let bubble = new H.ui.InfoBubble(event.target.getPosition(), {
+        content: event.target.getData()
+      });
+      this.ui.addBubble(bubble);
+    }, false);
+    this.map.addObject(marker);
   }
 
 }
