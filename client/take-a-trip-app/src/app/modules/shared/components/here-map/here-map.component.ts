b4acd8ffc69bf74a6ad929d5cddafed10dc6de4f
annix8 <mario.ivailov@abv.bg>
2019-07-14 22:12:59 +0300

    lat and lng as inputs in here map component

--- client/take-a-trip-app/src/app/modules/shared/components/here-map/here-map.component.ts
+++ client/take-a-trip-app/src/app/modules/shared/components/here-map/here-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
 
 declare var H: any;
 
@@ -8,38 +8,45 @@ declare var H: any;
   styleUrls: ['./here-map.component.scss']
 })
 export class HereMapComponent implements OnInit {
+  private platform: any;
   private ui: any;
   private map: any;
+
+  @Input() latitute: number;
+  @Input() longtitude: number;
   @ViewChild("map") public mapElement: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.platform = new H.service.Platform({
+      "apikey": "M00YI3E6cEVA4sMcOBo7LZXPwWMuzenfsU0JcB7QqmU"
+    });
   }
 
   public ngAfterViewInit() {
-    let platform = new H.service.Platform({
-      "apikey": "M00YI3E6cEVA4sMcOBo7LZXPwWMuzenfsU0JcB7QqmU"
-    });
-    let defaultLayers = platform.createDefaultLayers();
+    this.initMap();
+    this.dropMarker({ "lat": this.latitute, "lng": this.longtitude });    
+  }
+
+  private initMap() {
+    let defaultLayers = this.platform.createDefaultLayers();
     this.map = new H.Map(
       this.mapElement.nativeElement,
       defaultLayers.vector.normal.map,
       {
         zoom: 10,
-        center: { lat: 42.695808, lng: 23.332794 }
+        center: { lat: this.latitute, lng: this.longtitude }
       }
     );
+
     let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
     this.ui = H.ui.UI.createDefault(this.map, defaultLayers);
-
-    this.dropMarker({ "lat": 42.695808, "lng": 23.332794 }, "Saint Alexander Nevsky");
   }
 
-  private dropMarker(coordinates: any, title: string) {
+  private dropMarker(coordinates: any) {
     this.map.removeObjects(this.map.getObjects());
     let marker = new H.map.Marker(coordinates);
-    marker.setData("<p>" + title + "</p>");
     marker.addEventListener('tap', event => {
       let bubble = new H.ui.InfoBubble(event.target.getPosition(), {
         content: event.target.getData()
