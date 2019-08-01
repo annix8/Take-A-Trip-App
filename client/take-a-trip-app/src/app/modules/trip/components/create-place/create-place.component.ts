5254f584ef44e51990d144a9bea58adb5165a65f
annix8 <mario.ivailov@abv.bg>
2019-08-01 22:21:32 +0300

    cities are queried when creating place instead of visualized with a dropdown

--- client/take-a-trip-app/src/app/modules/trip/components/create-place/create-place.component.ts
+++ client/take-a-trip-app/src/app/modules/trip/components/create-place/create-place.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CityService } from 'src/app/services/city.service';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { ICity } from 'src/app/models/city';
 import { ICreatePlace } from 'src/app/models/create-place';
 import { PlaceService } from 'src/app/services/place.service';
 import { NgForm } from '@angular/forms';
 import { swalSuccess, swalError } from 'src/app/util/swal-util';
+import { debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-place',
@@ -14,6 +15,7 @@ import { swalSuccess, swalError } from 'src/app/util/swal-util';
 })
 export class CreatePlaceComponent implements OnInit {
   cities$: Observable<ICity[]>;
+  private searchTerms = new Subject<string>();
   createPlaceModel: ICreatePlace = {
     placeName: "",
     cityId: "",
@@ -23,12 +25,28 @@ export class CreatePlaceComponent implements OnInit {
     pictures: []
   };
   @ViewChild('form') public createPlaceForm: NgForm;
+  @ViewChild('citySearchInput') public citySearchInput: ElementRef;
 
   constructor(private cityService: CityService,
     private placeService: PlaceService) { }
 
   ngOnInit() {
-    this.cities$ = this.cityService.getAll();
+    this.cities$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.cityService.getAllByName(term)),
+      catchError(error => { console.log(error); return of([]) })
+    );
+  }
+
+  searchCity(searchValue: string){
+    this.searchTerms.next(searchValue);
+  }
+
+  selectCity(city: ICity){
+    this.createPlaceModel.cityId = city._id;
+    this.citySearchInput.nativeElement.value = `${city.name}, ${city.country}`;
+    this.searchTerms.next("");
   }
 
   getFileDetails(e) {
